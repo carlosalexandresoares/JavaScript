a9809fdf875200434ff82351bfaa77d81d8071ae
carlosalexandresoares <carlos.soares11@fatec.sp.gov.br>
2024-05-29 22:45:56 -0300

    operadores

---
+++ 2_OPERADORES/js/script.js
@@ -0,0 +1,117 @@
+// // 1 - Number
+// console.log(typeof 2);
+// console.log(typeof 5.13);
+// console.log(typeof -123);
+
+
+// // 2 operações aritmeticas
+
+// console.log(2 + 4);
+// console.log(10 - 5);
+// console.log(10 / 2);
+// console.log(10 * 5);
+// console.log(5 + (2 * 5));
+
+// // 3 special numbers
+
+// console.log(typeof Infinity);
+// console.log(typeof -Infinity);
+// console.log(12 * "asd");
+// console.log(typeof NaN);
+
+// // 4 Strings
+
+// console.log(typeof "teste")
+// console.log(typeof 'teste1')
+// console.log(typeof `teste`)
+
+// 5 Simbolos especiais em strings
+
+
+// console.log("Testando a \n quebra de linha")
+// console.log("Espaçamento \t de tab")
+
+// 6 Concatenação
+
+// console.log(" oi, " + "tudo" + " bem?");
+
+// 7 Templete Strings
+
+// console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+// console.log(`Podemos executar qualquer coisa aqui ${console.log("Teste")}`);
+
+// 8 Booleans
+
+// console.log(true);
+// console.log(false);
+
+// console.log(30 > 10);
+
+// console.log(typeof false);
+
+// 9 Comparação
+
+// /*
+// Maior e menor > e <
+
+// Maior ou igual Mneor ou igual >= e <=
+
+// igual ==
+
+// Diferente !=
+
+// Idêntico ===
+// */
+
+// console.log(5 <= 5);
+
+// console.log(5 < 5);
+
+// console.log(10 == 10);
+// console.log(10 == 9);
+
+// console.log(10 != 9);
+
+// 10 Isendico
+
+// console.log(9 == "9");
+// console.log(9 === "9");
+// console.log(9 != "9");
+// console.log(9 !== "9");
+
+// 11 operadores lógicos
+
+/*
+&& - AND - true apenas se os dois forem verdadeiros
+
+|| - OR - para ser true, um lado como true é o suficiente
+
+! - NOT este operador inverte a comparação
+
+*/
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(5 > 2 && 2 < 10);
+// console.log(5 > 2 && "Carlos" === 1);
+// console.log(5 > 2 || "Carlos" === 1);
+// console.log(5 < 2 || 5 > 100);
+
+// console.log(!true);
+
+// console.log(!5 > 2);
+
+// 12 empty value
+
+// console.log(typeof null, typeof undefined);
+// console.log(null === undefined);
+// console.log(null == undefined);
+// console.log(null == false);
+// console.log(undefined == false);
+
+// 13 mudança de tipos
+
+// console.log(5 * null);
+// console.log("teste" * "tess");
+// console.log("5" + 1);
+// console.log("5" - 1);
